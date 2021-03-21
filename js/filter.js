8568c115e35b4c5afa02b7cd4f503644690a3882
Anna Tolstikova <39711434+Asusey@users.noreply.github.com>
2021-03-21 23:01:42 +0300

    Чекбоксы, устранение дребезга

--- js/filter.js
+++ js/filter.js
@@ -1,58 +1,92 @@
-import { onFilterPins, ads } from './map.js';
+import { onFilterPins } from './map.js';
 
 const MAP_FILTERS = document.querySelector('.map__filters');
 const HOUSING_TYPE = MAP_FILTERS.querySelector('#housing-type');
 const HOUSING_PRICE = MAP_FILTERS.querySelector('#housing-price');
 const HOUSING_ROOMS = MAP_FILTERS.querySelector('#housing-rooms');
 const HOUSING_GUESTS = MAP_FILTERS.querySelector('#housing-guests');
-const HOUSING_FEATURES = MAP_FILTERS.querySelector('#housing-features');
+//const HOUSING_FEATURES = MAP_FILTERS.querySelector('#housing-features');
+const FILTER_CHECKBOX = MAP_FILTERS.querySelectorAll('.map__checkbox');
+const RERENDER_DELAY = 500;
 
+const initFilters = () => {
+  MAP_FILTERS.addEventListener(
+    'change',
+    window._.debounce(() => {
+      onFilterPins((ad) => {
+        if (
+          HOUSING_TYPE.value !== ad.offer.type &&
+          HOUSING_TYPE.value !== 'any'
+        ) {
+          return false;
+        }
 
+        if (
+          !(
+            (HOUSING_PRICE.value === 'low' && ad.offer.price < 10000) ||
+            (HOUSING_PRICE.value === 'high' && ad.offer.price > 50000) ||
+            (HOUSING_PRICE.value === 'middle' &&
+              ad.offer.price >= 10000 &&
+              ad.offer.price <= 50000) ||
+            HOUSING_PRICE.value === 'any'
+          )
+        ) {
+          return false;
+        }
 
-MAP_FILTERS.addEventListener('change', () => {
-  onFilterPins(ads => {
-    if (HOUSING_TYPE.value !== ads.offer.type && HOUSING_TYPE.value !== 'any') {
-      return false;
-    }
+        if (
+          HOUSING_ROOMS.value !== ad.offer.rooms.toString() &&
+          HOUSING_ROOMS.value !== 'any'
+        ) {
+          return false;
+        }
 
-    if (!(HOUSING_PRICE.value === 'low' && ads.offer.price < 10000 ||
-    HOUSING_PRICE.value === 'high' && ads.offer.price > 50000 ||
-    HOUSING_PRICE.value === 'middle' && ads.offer.price >= 10000 && ads.offer.price <= 50000 || HOUSING_PRICE.value === 'any')) {
-      return false;
-    }
+        if (
+          HOUSING_GUESTS.value !== ad.offer.guests.toString() &&
+          HOUSING_GUESTS.value !== 'any'
+        ) {
+          return false;
+        }
 
-    if (HOUSING_ROOMS.value !== ads.offer.rooms.toString() && HOUSING_ROOMS.value !== 'any') {
-      return false;
-    }
+        const requestedFeatures = Array.from(FILTER_CHECKBOX).filter(
+          (element) => element.checked,
+        );
+        if (
+          requestedFeatures.some(
+            (element) => !ad.offer.features.includes(element.value),
+          )
+        ) {
+          return false;
+        }
 
-    if (HOUSING_GUESTS.value !== ads.offer.guests.toString() && HOUSING_GUESTS.value !== 'any') {
-      return false;
-    }
-    return true;
-  });
+        //console.log(Array.from(FILTER_CHECKBOX));
+        return true;
+      });
+    }, RERENDER_DELAY),
+  );
+};
 
-
-});
+export { initFilters };
 
 // const getFilterByType = (type) => {
 //   HOUSING_TYPE.value === 'any' || type === HOUSING_TYPE.value;
 //}
 
-HOUSING_FEATURES.addEventListener('change', () => {
+// HOUSING_FEATURES.addEventListener('change', () => {
 
-  //console.log(evt);
-  const FILTER_CHECKBOX = MAP_FILTERS.querySelectorAll('.map__checkbox');
-  let result = {};
-  FILTER_CHECKBOX.forEach((element) => {
-    //console.log(element.checked);
-    ads.forEach((ad) => {
-      const isInclude = ad.offer.features.includes(element.value);
-      if (isInclude) {
-        result[ad.offer.address] = ad;
-      }
-    })
-  });
-  //console.log(result);
-  const filteredResult = Object.values(result);
-  console.log(filteredResult);
-});
+//   //console.log(evt);
+
+//   let result = {};
+//   FILTER_CHECKBOX.forEach((element) => {
+//     //console.log(element.checked);
+//     ads.forEach((ad) => {
+//       const isInclude = ad.offer.features.includes(element.value);
+//       if (isInclude) {
+//         result[ad.offer.address] = ad;
+//       }
+//     })
+//   });
+//   //console.log(result);
+//   const filteredResult = Object.values(result);
+//   //console.log(filteredResult);
+// });
--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@ import { initMap } from  './map.js';
 import { initValidation } from  './validation.js';
 import { initFormSubmit } from './form.js';
 
-import './filter.js';
+import { initFilters } from './filter.js';
 
 initDisabled();
 initValidation();
 initFormSubmit();
 initMap();
+initFilters();
